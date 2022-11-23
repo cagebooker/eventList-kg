f459c79e908637687f87dc87db7492c71b0f003a
KevinGu <cagebooker@gmail.com>
2022-11-23 11:34:52 +0800

    上傳大頭照 active storage

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -48,7 +48,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar])
   end
   def update_resource(resource, params)
     resource.update_without_password(params)
